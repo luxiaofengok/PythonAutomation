4171af2b93227cb4373f0ae950b5eb5c5eacd88d
luxiaofeng <pncuong1997@gmail.com>
2026-01-20 19:47:26 +0700

    fix github

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,15 +5,15 @@ from utils.config_reader import ConfigReader
 @pytest.fixture(scope="function")
 def driver():
     # day la phan setup cho cac test script va dung 1 lan duy nhat(before test)
-    driver=webdriver.Chrome()
-    driver.implicitly_wait(10)
-    driver.maximize_window()
+    # driver=webdriver.Chrome()
+    # driver.implicitly_wait(10)
+    # driver.maximize_window()
     options = webdriver.ChromeOptions()
     options.add_argument("--headless")  # Run headless Chrome
     driver = webdriver.Chrome(options=options)
-    #url="https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
+    url="https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
     #url="https://www.letskodeit.com/practice"
-    url=ConfigReader.get_base_url()
+    #url=ConfigReader.get_base_url()
     driver.get(url)
     yield driver #cho script chay het roi moi toi teardown(after test)
     
